21b45d04a2fc6fcdb69816434b6b5a22320c7e2d
FatCat0712 <maisonhai456@gmail.com>
2023-06-20 21:53:21 +0700

    update

--- Python_training/day1.py
+++ Python_training/day1.py
@@ -1,3 +1,9 @@
 print("Hello world")
-# win + .
+# win + . = icon
+# Shift + Alt + Down = nhan doi code
+# Alt + Up = di chuyen code
+# Ctrl + Shift + P = command palete
+# Ctrl + J = open terminal
 print("😂")
+print("😘😘😘😘😘")
+print("😘😘😘😘😘")
